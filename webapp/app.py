b1c8b20c92144204cd91f2ccb1a6b4ba9e35b605
pushpitasaha <pushpitasaha_2025@depauw.edu>
2025-05-02 00:09:42 -0400

    debug: change flask default from 127.0.0.1 inside container to listen on all network interfaces

--- webapp/app.py
+++ webapp/app.py
@@ -61,4 +61,4 @@ def index():
     return render_template("index.html", result=result, plot_url=plot_url)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host ='0.0.0.0')
